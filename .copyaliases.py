ca9a8243e811dce0ba06ac49ac6d61af76eda08b
Pilen <fiskomaten@gmail.com>
2011-12-07 18:17:37 +0100

    Configuration updated

--- .copyaliases.py
+++ .copyaliases.py
@@ -3,6 +3,6 @@
 with open('/home/pilen/.aliases','r') as inp:
     with open('/home/pilen/.emacs.d/eshell/alias','w') as out:
         for line in inp:
-            if line.startswith('alias'):
-                out.write(line.partition('=')[0] + " " + line.partition('=')[2][:-2]+' $*\'\n')
+            if line.startswith('alias '):
+                out.write(line.partition('=')[0] + " " + line.partition('=')[2][1:-2]+' $*\n')
         out.write("alias f 'find-file $1'\n")
