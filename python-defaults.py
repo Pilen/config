9eb1a9a16427bb1144e1e41f5264151e519e015d
Søren Pilgård <spi@isynet.net>
2018-04-03 11:24:16 +0200

    updated python-defaults

--- python-defaults.py
+++ python-defaults.py
@@ -1,10 +1,17 @@
-
 import sys
 import datetime
+from datetime import datetime as dt
 import re
 import subprocess
 import pathlib
+from pathlib import Path
 import random
+import collections
+import pprint
+from pprint import pprint as pp
+import itertools
+import functools
+import argparse
 
 d = {"a":"A", "b":"B", "c":"C"}
 l = [1, 2, 3]
