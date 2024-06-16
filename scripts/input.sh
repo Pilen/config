121673b5fd15e9202ad3bbdce922cda3c53659be
Pilen <fiskomaten@gmail.com>
2024-06-16 23:56:25 +0200

    Change from xmodmap to setxkbmap (to please new version of Emacs)

--- scripts/input.sh
+++ scripts/input.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
 
-setxkbmap us -variant colemak
+# setxkbmap us -variant colemak
+xkbcomp -w 0 -I$HOME/config/xkb ~/config/xkb/map $DISPLAY
 xset r 66
-xmodmap .Xmodmap
+# xmodmap .Xmodmap
 xsetroot -cursor_name left_ptr
 xset -b
 xset r rate 200 30
